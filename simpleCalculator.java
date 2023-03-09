ce4fab778a8582b5e66b1b415bbe090eab7ea647
IshaA1310 <127334292+IshaA1310@users.noreply.github.com>
2023-03-09 07:21:55 +0530

    Update simpleCalculator.java

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -3,16 +3,16 @@ class Main{
     public static void main(String[] args)
         {
               Scanner s = new Scanner(System.in);
-                    double num1= s.nextDouble();
-                          char opr = s.nextLine().charAt(0);
-                                double num2 = s.nextDouble();
-                                      double result=0;
+              double num1= s.nextDouble();
+              char opr = s.nextLine().charAt(0);
+              double num2 = s.nextDouble();
+              double result=0;
 
-                                            switch(opr){
-                                                      case '+':
-                                                                 result=(num1 + num2);
-                                                                            System.out.printf("%.2f",result);
-                                                                                       break;
+              switch(opr){
+                  case '+':
+                           result=(num1 + num2);
+                           System.out.printf("%.2f",result);
+                           break;
 
                                                                                                  case '-':
                                                                                                              result=(num1 - num2);
