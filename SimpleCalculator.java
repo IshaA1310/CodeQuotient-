6aafc9229cef57d12e3d570b98bd667c010c164b
IshaA1310 <127334292+IshaA1310@users.noreply.github.com>
2023-03-09 16:57:17 +0530

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args)
+        {
+          Scanner s = new Scanner(System.in);
+          double a = s.nextDouble();
+          char opr = s.next().charAt(0);
+          double b = s.nextDouble();
+          double result =0;
+          switch (opr){
+              case '+' :
+              result = (a+b);
+              break;
+              
+              case '-' : 
+              result = (a-b);
+              break ;
+              
+              case '*' :
+              result =(a*b);
+              break;
+              
+              case '/' :
+              result = (a/b);
+              break;
+              
+              default :
+               break ;}
+
+             // Decimal upto 2 places
+          System.out.printf("%.2f",result);
+        }
+    
+}
