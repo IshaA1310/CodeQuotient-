7153f2d3accd390dc95aafec8a1ab642f0f1f6d1
IshaA1310 <127334292+IshaA1310@users.noreply.github.com>
2023-03-16 08:24:11 +0530

    Create 6.counterDiagonal.java

---
+++ Arrays/6.counterDiagonal.java
@@ -0,0 +1,19 @@
+
+class Result {
+  static void counterDiagonal(int mat[][], int n) {
+    for(int col=0; col<n; col++){
+      int i=0, j=col;
+      while(i<n&&j>=0){
+        System.out.print(mat[i][j]+" ");
+        i++;
+        j--;}
+      }
+      for(int row=1; row<=n-1; row++){
+        int i=row;
+        int j=n-1;
+        while(i<n && j>=0){
+          System.out.print(mat[i][j]+" ");
+          i++;
+          j--;}}
+  }
+}
